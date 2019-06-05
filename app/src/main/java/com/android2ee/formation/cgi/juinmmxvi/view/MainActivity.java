4fe3c71150de0b8452f2d842ec023d541419b6c0
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2019-06-05 20:08:48 +0200

    First commit after migration of may 2019

--- app/src/main/java/com/android2ee/formation/cgi/juinmmxvi/view/MainActivity.java
+++ app/src/main/java/com/android2ee/formation/cgi/juinmmxvi/view/MainActivity.java
@@ -2,7 +2,7 @@ package com.android2ee.formation.cgi.juinmmxvi.view;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android2ee.formation.cgi.juinmmxvi.R;
 
